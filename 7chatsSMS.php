43eaf90136c504a271e648315e518b60fe5fe84d
Problem Solve <64337792+callsSolve@users.noreply.github.com>
2020-06-03 16:57:45 +0200

    Add files via upload

---
+++ 7chatsSMS.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Bizchats\7chatsSMS;
+
+use Bizchats\7chatsSMS\Drivers\Driver;
+
+class 7chatsSMS
+{   
+protected $config = array();
+    protected $smsDriver = null;
+    protected $voiceDriver = null;
+   
+    public function __construct(array $config)
+    {
+$this->config = $config;
+try {
+$default_sms_driver = $config['default_sms_driver'];
+$sms_config = $config['drivers'][$default_sms_driver]['sms'];
+$driverClass = 'Bizchats\\7chatsSMS\\Drivers\\'. ucfirst($default_sms_driver) . 'SMSDriver';
+$this->smsDriver = new $driverClass($sms_config);
+if (isset($config['default_voice_sms_driver']) && !empty($config['default_voice_sms_driver'])) {
+$default_voice_sms_driver = $config['default_voice_sms_driver'];
+if (isset($config['drivers'][$default_voice_sms_driver]['voice'])) {
+$voice_config = $config['drivers'][$default_voice_sms_driver]['voice'];
+$driverClass = 'Bizchats\\7chatsSMS\\Drivers\\'. ucfirst($default_voice_sms_driver) . 'VoiceDriver';
+$this->voiceDriver = new $driverClass($voice_config);
+} else {
+echo $default_voice_sms_driver . ' Voice configurations not set in 7chats-sms config file.';
+}
+}
+} catch (Exception $e) {
+echo 'Config Error: ' . $e->getMessage();
+}
+
+    }
+public function sendSMS(array $msg) 
+    {
+if (isset($msg['to']) && isset($msg['text'])) {
+//remove any + symbol before the phone number to allow the default drivers include it if required
+$msg['to'] = preg_replace("/\+/", '', $msg['to']); 
+// send request to service driver
+return $this->smsDriver->sendRequest($msg);
+} else {
+return [
+'status' => false,
+'statusCode' => Driver::MISSING_PARAMS,
+'statusDescription' => 'FROM, TO or TEXT is missing...',
+'data' => null,
+];
+}
+}   
+public function sendVoice(array $msg) 
+    {
+        //return $this->voiceDriver->sendRequest($msg);
+}  
+
+    
+}
